79370c915b80069424bce71015ccfe8b4b2fc086
Lucasboi <you@example.com>
2020-01-25 14:26:48 -0700

    Imported Color Sensor example code

--- 2020FRCsoftware/src/main/java/frc/robot/subsystems/ColorSensor.java
+++ 2020FRCsoftware/src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -10,16 +10,20 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import com.revrobotics.ColorSensorV3;
 
+
 /**
  * Add your docs here.
  */
+
 public class ColorSensor extends Subsystem {
+
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
   @Override
   public void initDefaultCommand() {
+
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    // setDefaultCommand(new initColorSensor());
   }
 }
