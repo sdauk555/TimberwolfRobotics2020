fa8be0218263186f761979bf58088935ac94eeb7
JaredAnicito <jranicito@eastmountainhigh.net>
2020-01-28 20:58:50 -0700

    Simplified the Commands

--- src/main/java/frc/robot/commands/ShooterSlow.java
+++ src/main/java/frc/robot/commands/ShooterSlow.java
@@ -7,42 +7,20 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 import frc.robot.Robot;
 
-public class ShooterSlow extends Command {
+public class ShooterSlow extends InstantCommand {
   public ShooterSlow() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
     requires(Robot.shooterSubsystem);
   }
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
-
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
     Robot.shooterSubsystem.speedDown();
-  }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return true;
-  }
-
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
   }
 }
