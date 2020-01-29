a6e4a8cab2569a94b12b5f8c12a8c2b1a5b10f7a
Lucasboi <you@example.com>
2020-01-28 21:12:52 -0700

    Fixed Commands

--- 2020FRCsoftware/src/main/java/frc/robot/commands/PrintColorCommand.java
+++ 2020FRCsoftware/src/main/java/frc/robot/commands/PrintColorCommand.java
@@ -14,24 +14,36 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot; 
 import com.revrobotics.ColorSensorV3;
 
-public class PrintColor extends Subsystem {
-  /**
-   * Change the I2C port below to match the connection of your color sensor
-   */
-  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
+public class PrintColorCommand extends Command {
+  public PrintColorCommand() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.driveSubsystem);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    //Robot.driveSubsystem.driveSystem(); becomes the future subsystem.
+
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
 
   /**
    * A Rev Color Sensor V3 object is constructed with an I2C port as a 
    * parameter. The device will be automatically initialized with default 
    * parameters.
    */
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-
-  @Override
-  public void robotPeriodic() {
+ // private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
     /**
      * The method GetColor() returns a normalized color value from the sensor and can be
      * useful if outputting the color to an RGB LED or similar. To
@@ -76,10 +88,21 @@ public class PrintColor extends Subsystem {
     */
   
   }
+    
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
 
-    @Override
-    protected void initDefaultCommand() {
-        // TODO Auto-generated method stub
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
 
-    }
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
 }
