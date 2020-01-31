0299c4eaafacd1a4e270acc4a99ad182a274fb50
JDavis999 <pipiboi@icloud.com>
2020-01-30 20:47:30 -0700

    Added speed constraints

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -37,18 +37,18 @@ public class Shooter extends Subsystem {
     double speedIncrease = .1;
     double currentSpeed = shooterMotor.get();
     double fastSpeed = currentSpeed + speedIncrease;
-    shooterMotor.set(ControlMode.PercentOutput, fastSpeed);
     if (fastSpeed > 1.0){
       fastSpeed = 1.0;}
+    shooterMotor.set(ControlMode.PercentOutput, fastSpeed);
   }
 
   public void speedDown() {
     double speedDecrease = -.1;
     double currentSpeed = shooterMotor.get();
     double slowSpeed = currentSpeed + speedDecrease;
-    shooterMotor.set(ControlMode.PercentOutput, slowSpeed);
     if (slowSpeed < -1.0) {
       slowSpeed = -1.0;}
+    shooterMotor.set(ControlMode.PercentOutput, slowSpeed);
   }
 
   public void shooterStop() {
