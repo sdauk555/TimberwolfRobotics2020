394361839531d1cab9c18c13d6c222230fb6b121
Ironispain <46803139+Ironispain@users.noreply.github.com>
2020-02-10 16:22:35 -0700

    controllers

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SolenoidBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
@@ -23,28 +24,57 @@ import frc.robot.RobotMap;
 public class Pneumatics extends Subsystem {
     private static final Compressor comp = new Compressor(0);
     private static final XboxController controller1 = new XboxController(RobotMap.controller1);
+    private static final XboxController controller2 = new XboxController(RobotMap.controller2);
     private static final DoubleSolenoid feederSolenoid = new DoubleSolenoid(RobotMap.hammerPneumaticsForward, RobotMap.hammerPneumaticsBackward);
     private static final DoubleSolenoid armSolenoid = new DoubleSolenoid(RobotMap.armsPneumaticsForward, RobotMap.armsPneumaticsBackward);
-
-    public void start(){
+    public static final //PressureSwitch pSwitch = new PressureSwitch(1);
+ 
+    void start() {
         Pneumatics.comp.start();
-        Pneumatics.comp.setClosedLoopControl(true);
+        Pneumatics.comp.getCompressorCurrent();
         
-    }
-//runs compressor
+        //if (pSwitch == 90){
+        //    Pneumatics.comp.start();
+        //}
 
-public void feederDeployment(){
+        //if (pSwitch == 120){
+        //    Pneumatics.comp.stop();
+        //}
+    }
+//runs compressor, Contains Pseudocode. This should be about right however I can't look up the proper syntax as I am currently,
+// in the Colorado maountians for hockey and don't even have service.
+
+//Note 1: pSwitch must be a double or an int for this code to work, otherwise I'll have to adjust the code a bit to make it compatable
+//          with booleans. Can NOT be a value!
+
+//Note 2: The code below this note still needs testing, however I don't have a controller availiable(see note abouve for details) and
+//          therefore can't test if the if statements will actually bind the buttons to the solenoids.
+
+//Note 3: Have Shane and/or Sam Proofread this code to make sure that a) it works and b) the titles and buttons I am using don't conflict anywhere else
+
+    public void feederDeployment(){
+        Double triggerL = controller1.getTriggerAxis(Hand.kLeft);
+
+        if (triggerL == 1){
         feederSolenoid.set(DoubleSolenoid.Value.kForward);
-}
+        }
+    }
 //moves pistons forward
 
-public void feederRetract(){
-    feederSolenoid.set(DoubleSolenoid.Value.kReverse);
+
+    public void feederRetract() {
+        Double triggerL = controller1.getTriggerAxis(Hand.kLeft);
+
+        if (triggerL == 0){
+        feederSolenoid.set(DoubleSolenoid.Value.kReverse);
+        }
 }
 //moves pistons backward
 
 public void off(){
-    if(controller1.getRawButtonReleased(2)) {
+    int Pov = controller1.getPOV();
+
+    if(Pov == 270){
         feederSolenoid.set(Value.kOff);
     }
 }
@@ -53,17 +83,29 @@ public void off(){
 ////////////////////////////////////////////////////////////////////////////////
 
 public void ArmsRaise(){
+    Double triggerR = controller1.getTriggerAxis(Hand.kRight);
+
+    if (triggerR == 1) {
     armSolenoid.set(DoubleSolenoid.Value.kForward);
+    }
 }
 //Raises arms
 
 public void ArmsRetract(){
+    Double triggerR = controller1.getTriggerAxis(Hand.kRight);
+
+    if (triggerR == 0){
     armSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
 }
 // lowers arms
 
 public void stop(){
-    armSolenoid.set(Value.kOff);
+        int Pov = controller1.getPOV();
+    
+        if(Pov == 270){
+            armSolenoid.set(Value.kOff);
+        }
 }
 //Stops pistons
 
