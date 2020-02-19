4c0ce10f22f86750d2cd3826497811d5e967c358
sdauk555 <samdauk@outlook.com>
2020-02-18 21:01:56 -0700

    started compressro in test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -105,5 +106,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+    final Compressor comp = new Compressor(0);
+    comp.start();
   }
 }
