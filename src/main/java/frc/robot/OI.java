63a8707cd863a01a86f32a4e40b090bd2a85f5c2
JaredAnicito <jranicito@eastmountainhigh.net>
2020-02-19 20:57:16 -0700

    Changed Hopper to be time based

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -29,7 +29,7 @@ public class OI {
 		operatorButtonA.whenPressed(new ShooterRun());
 		operatorButtonX.whenPressed(new ShooterStop());
 		operatorButtonY.whenPressed(new HopperMotorRun());
-		operatorButtonY.whenReleased(new HopperMotorStop());
+		
 		//operatorButtonA.whenPressed(new SelectGreen());
 		//operatorButtonB.whenPressed(new SelectRed());
 		//operatorButtonX.whenPressed(new SelectBlue());
--- src/main/java/frc/robot/commands/HopperMotorRun.java
+++ src/main/java/frc/robot/commands/HopperMotorRun.java
@@ -2,21 +2,30 @@ package frc.robot.commands;
 
 import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.Timer;
+
+
 
 public class HopperMotorRun extends InstantCommand {
-
+    
+    private final Timer m_timer = new Timer();
+    
     public HopperMotorRun(){
         requires(Robot.hopperSubsystem);
     }
 
     @Override
     protected void execute() {
-        Robot.hopperSubsystem.run();
-    
+        m_timer.start();
+        if (m_timer.get() < 2.0) {
+                Robot.hopperSubsystem.run(); 
+            } else {
+                Robot.hopperSubsystem.stop();
+       } 
     }
-
+    
     @Override
-  protected void interrupted() {
-  }
+    protected void interrupted() {
+    }
 
 }
