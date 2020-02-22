f48e3bf242dd198ac1de22f2e0b51bfe1933b4d2
sdauk555 <samdauk@outlook.com>
2020-02-22 11:30:54 -0700

    did some reformats

--- src/main/java/frc/robot/commands/HopperSequential.java
+++ src/main/java/frc/robot/commands/HopperSequential.java
@@ -1,21 +1,10 @@
 package frc.robot.commands;
 
-import frc.robot.Robot;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-
-
-
 public class HopperSequential extends SequentialCommandGroup {
-    
-    
+
     public HopperSequential() {
-       addCommands(
-           new HopperMotorRun().withTimeout(1),
-           new HopperMotorStop().withTimeout(3));
-      
+        addCommands(new HopperMotorRun().withTimeout(1), new HopperMotorStop().withTimeout(3));
     }
-
-    
-
 }
