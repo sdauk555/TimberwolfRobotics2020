81daf07bda1b0fddedc3c71a177d44d47c338c10
JaredAnicito <jranicito@eastmountainhigh.net>
2020-02-22 12:13:57 -0700

    Added MAXCOUNT, count, and holdMidX variables

--- src/main/java/frc/robot/commands/AlignRobot.java
+++ src/main/java/frc/robot/commands/AlignRobot.java
@@ -11,6 +11,10 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Robot;
 
 public class AlignRobot extends InstantCommand {
+    private final static int MAXCOUNT = 50;
+    private double holdMidX;
+    private int count = MAXCOUNT;
+
     /**
      * Creates a new AlignRobot.
      */
@@ -28,10 +32,21 @@ public class AlignRobot extends InstantCommand {
     public void execute() {
         double midX = Robot.visionSubsystem.getMidPointX();
         boolean resultscheck = Robot.visionSubsystem.getResultsCheck();
-        
+
         if (resultscheck == false) {
-            return;
-        } else if (midX < -0.1) {
+            if (count < MAXCOUNT) {
+                midX = holdMidX;
+                count = count + 1;
+            } else {
+                Robot.driveSubsystem.driveSystem(0, 0);
+                return;
+            }
+        } else {
+            holdMidX = midX;
+            count = 0;
+        }
+
+        if (midX < -0.1) {
             Robot.driveSubsystem.driveSystem(0, 0.1);
         } else if (midX > 0.1) {
             Robot.driveSubsystem.driveSystem(0, -0.1);
