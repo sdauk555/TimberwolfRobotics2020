66f73565e49a877f904069f44f3b9cb6035a2fe6
sdauk555 <samdauk@outlook.com>
2020-02-22 12:02:55 -0700

    fixed more default cmd errors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -48,7 +48,7 @@ public class Robot extends TimedRobot {
     
     hopperSubsystem.setDefaultCommand(new HopperMotorStop());
     driveSubsystem.setDefaultCommand(new DriveCommand());
-    feederSubsystem.setDefaultCommand(new FeederSolenoidOff());
+    feederSubsystem.setDefaultCommand(new FeederStop());
     
   }
 
