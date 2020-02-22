0e52f81afb66e1020529a76faaed60a82c1fb7a7
sdauk555 <samdauk@outlook.com>
2020-02-22 12:00:51 -0700

    fixed issues with default cmd

--- src/main/java/frc/robot/commands/FeederStop.java
+++ src/main/java/frc/robot/commands/FeederStop.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,25 +7,37 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-/**
- * Add your docs here.
- */
-public class FeederStop extends InstantCommand {
+public class FeederStop extends CommandBase {
   /**
-   * Add your docs here.
+   * Creates a new FeederStop.
    */
   public FeederStop() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(Robot.feederSubsystem);
   }
 
-  // Called repeatedly when this Command is scheduled to run
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     Robot.feederSubsystem.stop();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+ 
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
   }
 }
