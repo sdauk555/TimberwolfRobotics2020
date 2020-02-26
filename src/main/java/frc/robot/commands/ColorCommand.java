e467bc136ea6e7836ac8b80bf78789031d1c1727
Lucasboi <you@example.com>
2020-02-25 21:27:11 -0700

    fixed errors in ColorCommand

--- src/main/java/frc/robot/commands/ColorCommand.java
+++ src/main/java/frc/robot/commands/ColorCommand.java
@@ -32,13 +32,8 @@ public class ColorCommand extends CommandBase {
   
     // Called once after isFinished returns true
     @Override
-    public void end() {
+    public void end(boolean interrupted) {
     }
   
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    public void interrupted() {
-    }
   }
   
