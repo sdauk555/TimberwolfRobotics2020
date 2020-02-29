b20d31ef354ad26c9de5b7c21013fc3041dcc121
sdauk555 <samdauk@outlook.com>
2020-02-29 09:34:14 -0700

    renamed hopperParallel to activateHopper

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -28,7 +28,7 @@ public class OI {
 		driverButtonB.whenPressed(new ControlPanelMotorStart());
 		driverButtonY.whenPressed(new ControlPanelMotorStop());
 		
-		operatorButtonRightBumper.whileHeld(new HopperParallel());
+		operatorButtonRightBumper.whileHeld(new ActivateHopper());
 		operatorButtonLeftBumper.whileHeld(new ShooterRun());
 		
 		//operatorButtonA.whenPressed(new SelectGreen());
