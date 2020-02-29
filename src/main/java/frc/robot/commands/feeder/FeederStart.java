1f0767afdd7753ea0e142ef3f594869aefbf32f7
sdauk555 <samdauk@outlook.com>
2020-02-29 15:54:54 -0700

    changed instant cmds to regular

--- src/main/java/frc/robot/commands/feeder/FeederStart.java
+++ src/main/java/frc/robot/commands/feeder/FeederStart.java
@@ -1,30 +1,43 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
+
 package frc.robot.commands.feeder;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-/**
- * Add your docs here.
- */
-public class FeederStart extends InstantCommand {
-    /**
-     * Add your docs here.
-     */
-    public FeederStart() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-        addRequirements(Robot.feederSubsystem);
-    }
+public class FeederStart extends CommandBase {
+  /**
+   * Creates a new FeederStart.
+   */
+  public FeederStart() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.feederSubsystem);
+  }
 
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    public void execute() {
-        Robot.feederSubsystem.start();
-    }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Robot.feederSubsystem.start();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
