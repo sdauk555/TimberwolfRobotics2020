732e351ef18185e857c231d0b6eed4f2f2607bdc
unknown <jranicito@eastmountainhigh.net>
2020-03-04 20:25:25 -0700

    changed shooterstop to cmd

--- src/main/java/frc/robot/commands/shooter/ShooterStop.java
+++ src/main/java/frc/robot/commands/shooter/ShooterStop.java
@@ -7,13 +7,13 @@
 
 package frc.robot.commands.shooter;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
-public class ShooterStop extends InstantCommand {
+public class ShooterStop extends CommandBase {
   public ShooterStop() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(Robot.shooterSubsystem);
