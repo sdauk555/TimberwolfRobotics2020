13577efe50c25a96b9cade15fafc900a04d5f84e
sdauk555 <samdauk@outlook.com>
2020-03-04 19:46:09 -0700

    changed shooterstop to instant cmd

--- src/main/java/frc/robot/commands/shooter/ShooterStop.java
+++ src/main/java/frc/robot/commands/shooter/ShooterStop.java
@@ -7,37 +7,23 @@
 
 package frc.robot.commands.shooter;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Robot;
 
-public class ShooterStop extends CommandBase {
-  /**
-   * Creates a new ShooterStop.
-   */
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class ShooterStop extends InstantCommand {
   public ShooterStop() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(Robot.shooterSubsystem);
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-  }
-  
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
   public void execute() {
     Robot.shooterSubsystem.shooterStop();
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
