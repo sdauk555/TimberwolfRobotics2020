faf4d6b2b36fd3803042e189e3950f44dac78a6a
sdauk555 <samdauk@outlook.com>
2020-03-04 20:10:26 -0700

    got rid of triggerShoot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,7 +21,6 @@ import frc.robot.commands.feeder.FeederStop;
 import frc.robot.commands.hopper.AgitatorStop;
 import frc.robot.commands.hopper.HopperMotorStop;
 import frc.robot.commands.shooter.Shoot;
-import frc.robot.commands.shooter.TriggerShoot;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -68,7 +67,7 @@ public class Robot extends TimedRobot {
 
     hopperSubsystem.setDefaultCommand(new HopperMotorStop());
     driveSubsystem.setDefaultCommand(new DriveCommand());
-    shooterSubsystem.setDefaultCommand(new TriggerShoot());
+    shooterSubsystem.setDefaultCommand(new Shoot());
     controlpanelSubsystem.setDefaultCommand(new ControlPanelMotorStop());
     feederSubsystem.setDefaultCommand(new FeederStop());
 
