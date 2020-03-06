ad732f68ac01e8b60fa605605abbf8380c2a5b6c
JDavis999 <pipiboi@icloud.com>
2020-03-05 18:49:38 -0700

    Control Panel Stage One

--- src/main/java/frc/robot/commands/control_panel/ControlPanelStageOne.java
+++ src/main/java/frc/robot/commands/control_panel/ControlPanelStageOne.java
@@ -8,7 +8,7 @@ import frc.robot.Robot;
 public class ControlPanelStageOne extends SequentialCommandGroup {
 
     public ControlPanelStageOne() {
-        addCommands(new ControlPanelExtend(), new ControlPanelMotorStart(), new WaitCommand(3.0),
-                new ControlPanelMotorStop(), new ControlPanelContract().withTimeout(3.0));
+        addCommands(new ControlPanelMotorStart(), new WaitCommand(3.0),
+                new ControlPanelMotorStop());
     }
 }
